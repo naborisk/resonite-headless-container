6272394401f794265ab53eaed89d3c0f06bba521
Naborisk <github@naborisk.com>
2024-08-23 22:46:29 +0900

    add license request

--- download-headless.sh
+++ download-headless.sh
@@ -2,4 +2,8 @@
 
 source .env
 
-steamcmd +force_install_dir /Resonite +login ${STEAM_USER} ${STEAM_PASS} +app_update 2519830 -beta headless -betapassword ${BETA_PASSWORD} validate +quit
+steamcmd +force_install_dir /Resonite \
+  +login ${STEAM_USER} ${STEAM_PASS} \
+  +app_license_request 2519830 \
+  +app_update 2519830 -beta headless -betapassword ${BETA_PASSWORD} validate \
+  +quit

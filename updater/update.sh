342399959dc22999ec6a13bb72c901052abcea3b
Naborisk <github@naborisk.com>
2024-09-12 23:51:57 +0900

    add logs

--- updater/update.sh
+++ updater/update.sh
@@ -5,6 +5,9 @@ echo CHECK_TIME_UPDATED: $CHECK_TIME_UPDATED
 echo LAST_UPDATED: $LAST_UPDATED
 
 if [ "$CHECK_TIME_UPDATED" != "$LAST_UPDATED" ]; then
+  echo "New update found, updating..., last updated: $LAST_UPDATED -> $CHECK_TIME_UPDATED"
   gh workflow run build.yaml --ref main
   gh variable set LAST_UPDATED $CHECK_TIME_UPDATED
+else
+  echo "No need to update, last updated: $LAST_UPDATED -> $CHECK_TIME_UPDATED"
 fi

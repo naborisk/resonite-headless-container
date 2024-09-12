8342b6684a04739cdfa14ee735f4db7757269310
Naborisk <github@naborisk.com>
2024-09-12 23:44:22 +0900

    a

--- updater/update.sh
+++ updater/update.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 CHECK_TIME_UPDATED=$(curl https://api.steamcmd.net/v1/info/2519830 | jq -r '.data."2519830".depots.branches.headless.timeupdated')
 
-echo $CHECK_TIME_UPDATED
-echo $LAST_UPDATED
-# gh workflow run build.yaml --ref main
+echo CHECK_TIME_UPDATED: $CHECK_TIME_UPDATED
+echo LAST_UPDATED: $LAST_UPDATED
+
+# if [ "$CHECK_TIME_UPDATED" != "$LAST_UPDATED" ]; then
+#   gh workflow run build.yaml --ref main
+#   gh variable set LAST_UPDATED $CHECK_TIME_UPDATED
+# fi

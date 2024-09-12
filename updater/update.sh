30edf6d7f27dcb6a334c72bf1e01f30bdf46dd63
Naborisk <github@naborisk.com>
2024-09-12 23:36:20 +0900

    WIP add check-update, remove nightly

--- updater/update.sh
+++ updater/update.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-curl https://api.steamcmd.net/v1/info/2519830 | jq -r '.data."2519830".depots.branches.headless.timeupdated'
+CHECK_TIME_UPDATED=$(curl https://api.steamcmd.net/v1/info/2519830 | jq -r '.data."2519830".depots.branches.headless.timeupdated')
+
+echo $CHECK_TIME_UPDATED
+echo $LAST_UPDATED
+# gh workflow run build.yaml --ref main

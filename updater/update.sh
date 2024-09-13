296994db464485cbb025d61032ddfbbe99019023
Naborisk <github@naborisk.com>
2024-09-13 13:39:26 +0900

    silence curl

--- updater/update.sh
+++ updater/update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-CHECK_TIME_UPDATED=$(curl https://api.steamcmd.net/v1/info/2519830 | jq -r '.data."2519830".depots.branches.headless.timeupdated')
+CHECK_TIME_UPDATED=$(curl -s https://api.steamcmd.net/v1/info/2519830 | jq -r '.data."2519830".depots.branches.headless.timeupdated')
 
 echo CHECK_TIME_UPDATED: $CHECK_TIME_UPDATED
 echo LAST_UPDATED: $LAST_UPDATED
@@ -7,7 +7,7 @@ echo LAST_UPDATED: $LAST_UPDATED
 if [ "$CHECK_TIME_UPDATED" != "$LAST_UPDATED" ] && [ "$CHECK_TIME_UPDATED" != null ]; then
   echo "New update found, updating..., last updated: $LAST_UPDATED -> $CHECK_TIME_UPDATED"
 
-  curl -L \
+  curl -sL \
     -X POST \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $GH_TOKEN" \
@@ -15,7 +15,7 @@ if [ "$CHECK_TIME_UPDATED" != "$LAST_UPDATED" ] && [ "$CHECK_TIME_UPDATED" != nu
     https://api.github.com/repos/naborisk/resonite-headless-container/actions/workflows/build.yaml/dispatches \
     -d '{"ref":"main"}'
 
-  curl -L \
+  curl -sL \
     -X PATCH \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $PAT" \

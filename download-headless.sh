e792f8de9ec62ec4b5b324b38c0920f86adf954f
Naborisk <github@naborisk.com>
2025-08-12 17:46:50 +0900

    add prelease option to build

--- download-headless.sh
+++ download-headless.sh
@@ -5,5 +5,5 @@ source .env
 steamcmd +force_install_dir /Resonite \
   +login ${STEAM_USER} ${STEAM_PASS} \
   +app_license_request 2519830 \
-  +app_update 2519830 -beta headless -betapassword ${BETA_PASSWORD} validate \
+  +app_update 2519830 -beta ${STEAM_BETA} -betapassword ${BETA_PASSWORD} validate \
   +quit
